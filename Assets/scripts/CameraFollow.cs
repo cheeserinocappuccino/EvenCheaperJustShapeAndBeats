8f374773ed44bfeeecb175a76e7968ec971620c7
DESKTOP-OAKDG8P\張澤瑜 <a106082016@mail.shu.edu.tw>
2018-10-19 01:55:26 +0800

    2018/10/19 草稿

---
+++ Assets/scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public GameObject Target;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = Target.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position += (Target.transform.position - (transform.position + offset)) / 25;
+        transform.position += (new Vector3(0, Target.transform.position.y, 0) - new Vector3(0, transform.position.y, 0 ))/25;
+
+    }
+}
