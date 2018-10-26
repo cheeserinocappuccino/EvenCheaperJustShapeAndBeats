f12bca30126565947d1be27822f486ed9190607b
DESKTOP-OAKDG8P\張澤瑜 <a106082016@mail.shu.edu.tw>
2018-10-26 12:07:04 +0800

    10/26

--- Assets/scripts/PlayerBehave.cs
+++ Assets/scripts/PlayerBehave.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBehave : MonoBehaviour {
+    public int healthPoint ;
+
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +13,16 @@ public class PlayerBehave : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (healthPoint <= 0)
+        {
+            Debug.Log("玩家扣血");
+            Destroy(this.gameObject);
+        }
 	}
+
+    public void Damaged(int amount)
+    {
+        healthPoint -= amount;
+
+    }
 }
